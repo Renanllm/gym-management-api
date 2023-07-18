a0215a56ce8cb5b17d9db439b7a902cb60055f87
Renanllm <renanllm01@gmail.com>
2023-07-18 14:06:09 -0300

    Update endpoint message

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,6 @@ import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
   app.get('/', (request, response) => {
-    response.send('Hello World!')
+    response.send('Hello from AWS EC2 instance!')
   })
 }
