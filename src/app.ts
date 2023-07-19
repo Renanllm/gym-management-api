c27368226a414c9c203cd30b95d6d98621d4b68b
Renanllm <renanllm01@gmail.com>
2023-07-19 11:09:58 -0300

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
 import { errorHandler } from './http/handlers/error-handler'
+import cors from '@fastify/cors'
 
 export const app = fastify()
+
+// should configure cors to allow only the frontend url
+app.register(cors)
 
 app.register(appRoutes)
 
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,13 @@ import { prisma } from '@/lib/prisma'
 import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/', (request, response) => {
-    response.send('Hello from AWS EC2 instance!')
+  app.get('/', (request, reply) => {
+    reply.send('Hello from AWS EC2 instance!')
   })
 
-  app.get('/students', async (request, response) => {
-    return await prisma.student.findMany()
+  app.get('/students', async (request, reply) => {
+    console.log('chegou')
+    const students = await prisma.student.findMany()
+    reply.send({ students })
   })
 }
